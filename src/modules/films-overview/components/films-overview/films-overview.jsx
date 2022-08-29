b2fefbdb18cd9e2b033137ad1f309d1d4e8da72a
Ahmed Abbas <ahmed.n.abbas@gmail.com>
2022-08-29 21:50:10 +0200

    skip using translations in ssg

--- src/modules/films-overview/components/films-overview/films-overview.jsx
+++ src/modules/films-overview/components/films-overview/films-overview.jsx
@@ -1,15 +1,15 @@
 import classnames from 'classnames'
-import { useTranslation } from 'next-export-i18n'
+// import { useTranslation } from 'next-export-i18n'
 import FilmBlock from '../film-block/film-block'
 import { useUser } from 'common/contexts'
 import styles from './films-overview.module.scss'
 
 const FilmsOverview = ({ films }) => {
   const { lang } = useUser()
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
   return (
     <>
-      <h2>{t('home.h2')}</h2>
+      <h2>Bekeken films</h2>
       <ul className={classnames(styles['films-overview'])}>
         {films.map((film) => (
           <li key={`${film.identifier}`}>
