e676046d41e2c86a88d97c39a2bf9fb13f265193
Ahmed Abbas <ahmed.n.abbas@gmail.com>
2022-08-30 22:57:33 +0200

    add films

--- src/modules/films-overview/components/films-overview/films-overview.functions.js
+++ src/modules/films-overview/components/films-overview/films-overview.functions.js
@@ -5,17 +5,20 @@ export const sortFilms = (films, type, isAscending = true) => {
     case SORT_DATE:
       return sortByDate(films, isAscending)
     case SORT_TITLE:
-      console.log('BY TITLE!')
       return sortByTitle(films, isAscending)
   }
 }
 
 const sortByTitle = (films, isAscending) => {
   return [...films].sort((previous, next) => {
+    // if film has sortableTitle property, use that
+    const previousTitle = previous.sortableTitle || previous.title
+    const nextTitle = next.sortableTitle || next.title
+
     if (isAscending) {
-      return previous.title.localeCompare(next.title)
+      return previousTitle.localeCompare(nextTitle)
     }
-    return next.title.localeCompare(previous.title)
+    return nextTitle.localeCompare(previousTitle)
   })
 }
 
