1277d4b41f24fd9f16f1169ed543ce9143eb34b6
Ahmed Abbas <ahmed.n.abbas@gmail.com>
2022-08-30 17:08:51 +0200

    remove i18n from next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: '/cineville',
-  i18n: {
-    locales: ['nl', 'en'],
-    defaultLocale: 'nl',
-    localeDetection: false,
-  },
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
