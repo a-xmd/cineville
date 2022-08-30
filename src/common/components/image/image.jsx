335423b8c08222d3e8db4562b6f29088f9fec70f
Ahmed Abbas <ahmed.n.abbas@gmail.com>
2022-08-30 18:49:33 +0200

    experiment: use 100vw for sizes

--- src/common/components/image/image.jsx
+++ src/common/components/image/image.jsx
@@ -7,10 +7,26 @@ const Image = ({ imageName, alt, ext = 'jpg' }) => {
 
   return (
     <picture className={classnames(styles.image)}>
-      <source media="(max-width: 480px)" srcSet={`${src}-480.${ext} 480w`} />
-      <source media="(max-width: 768px)" srcSet={`${src}-768.${ext} 768w`} />
-      <source media="(max-width: 1152px" srcSet={`${src}-1152.${ext} 1152w`} />
-      <source media="(max-width: 1440" srcSet={`${src}-1440.${ext} 1440w`} />
+      <source
+        media="(max-width: 480px)"
+        srcSet={`${src}-480.${ext} 480w`}
+        sizes="100vw"
+      />
+      <source
+        media="(max-width: 768px)"
+        srcSet={`${src}-768.${ext} 768w`}
+        sizes="100vw"
+      />
+      <source
+        media="(max-width: 1152px"
+        srcSet={`${src}-1152.${ext} 1152w`}
+        sizes="100vw"
+      />
+      <source
+        media="(max-width: 1440"
+        srcSet={`${src}-1440.${ext} 1440w`}
+        sizes="100vw"
+      />
 
       <img src={`${src}-1440.${ext}`} alt={alt} />
     </picture>
